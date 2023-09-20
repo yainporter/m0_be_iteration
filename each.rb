16e8051c80adb80bc2787ae8552927f8c21de4e8
Yain Porter <yainporter@gmail.com>
2023-09-20 10:35:42 -0700

    Complete Challenge 1 & 2 of the each method as well as the self evaluation

--- each.rb
+++ each.rb
@@ -5,10 +5,17 @@
 # Challenge 1
 # Use the each method to print out a complete sentence stating the weather using the Array below:
 weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+weather_options.each do |weather_option|
+  puts "Today's weather is #{weather_option}!"
+end
 
 # Challenge 2
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
-
+words.each do |word|
+  if word.length == 3
+    p word
+  end
+end
 
 # Record yourself live-coding and talking through either Challenge 1 or 2. The recording doesn't have to be the first time you are solving the problem. Use technical vocabulary and explain each part to demonstrate your understanding! Share the link to your video in your small group channel.
--- self-evaluation.md
+++ self-evaluation.md
@@ -1,6 +1,9 @@
 # Self Evaluation
 
 - How might the `each` method be useful?
+It's very useful when you have a list of arracys that you need to repeat, but you want to put specific methods on what is repeated.
 - What do you still need to practice or learn? How will you do that?
+I just need to get used to using the correct syntax for the `each` method. It just takes practice.
 - What worked well for you in terms of your approach on this
 assignment?
+It helps to have the browser up with the lesson so that I can go back and reference code when I need it.
